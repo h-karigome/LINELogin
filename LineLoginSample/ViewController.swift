9986ad1de85246eae7e0a909f619279a7c359385
h-karigome <h-karigome@i-enter.co.jp>
2019-03-13 14:14:48 +0900

    Initial Commit

---
+++ LineLoginSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LineLoginSample
+//
+//  Created by 苅米 春奈 on 2019/03/13.
+//  Copyright © 2019 苅米 春奈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LineLoginSampleTests/LineLoginSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  LineLoginSampleTests.swift
+//  LineLoginSampleTests
+//
+//  Created by 苅米 春奈 on 2019/03/13.
+//  Copyright © 2019 苅米 春奈. All rights reserved.
+//
+
+import XCTest
+@testable import LineLoginSample
+
+class LineLoginSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
